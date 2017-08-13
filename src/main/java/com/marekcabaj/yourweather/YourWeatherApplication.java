e97403638e9c78e72bd33ae3e2fe6d7d1bd31f16
marek.cabaj <marek.cbj@gmail.com>
2017-08-13 16:04:04 +0200

    Generated Spring Boot project

---
+++ src/main/java/com/marekcabaj/yourweather/YourWeatherApplication.java
@@ -0,0 +1,12 @@
+package com.marekcabaj.yourweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourWeatherApplication.class, args);
+	}
+}
---
+++ src/test/java/com/marekcabaj/yourweather/YourWeatherApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marekcabaj.yourweather;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YourWeatherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
