9990e0f7a22e50c79a53f087182017600fe5d984
marek.cabaj <marek.cbj@gmail.com>
2017-08-13 16:47:52 +0200

    Static directory changes

--- src/main/java/com/marekcabaj/controllers/WeatherController.java
+++ src/main/java/com/marekcabaj/controllers/WeatherController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WeatherController {
 
-    @RequestMapping("/index")
+    @RequestMapping("/")
     public String mainPage() {
         return "index";
     }
